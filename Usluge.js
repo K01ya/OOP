972ee93be739486d6266fb0b0d0464c4ff3d5250
K01ya <101926916+K01ya@users.noreply.github.com>
2022-06-22 20:42:38 +0200

    Add files via upload

---
+++ Usluge.js
@@ -0,0 +1,60 @@
+function testfunction1() {
+    
+ let igraonica = {
+   Usluga1 : [
+     {
+    usluga1: "Kompjuter",
+		igrica1:[ { 
+			naziv:"CS:GO", 
+			godizbacivanja:"2012", 
+			zanr:"SHOOTER",
+			cena: "1000din"
+		} ],
+		igrica2:[ { 
+			naziv:"LOL", 
+			godizbacivanja:"2005", 
+			zanr:"MMORPG",
+			cena: "0din"
+		} ],
+		igrica3:[ { 
+			naziv:"GTA5", 
+			godizbacivanja:"2015", 
+			zanr:"ROLE-PLAY",
+			cena: "5000din"
+		} ],
+		igrica4:[ { 
+			naziv:"MINESWEEPER", 
+			godizbacivanja:"1989", 
+			zanr:"CASUAL",
+			cena: "0din"
+		} ]
+     }
+   ],
+   
+   Usluga2 : [
+    {
+   usluga2: "PS4",
+	   igrica1:[ { 
+				naziv:"GOD OF WAR", 
+				godizbacivanja:"1999", 
+				zanr:"ACTION",
+				cena: "1500din"
+			} ],
+			igrica2:[ { 
+				naziv:"NEED FOR SPEED", 
+				godizbacivanja:"2001", 
+				zanr:"RACING",
+				cena: "1200din"
+			} ],
+			igrica3:[ { 
+				naziv:"GTA5", 
+				godizbacivanja:"2015", 
+				zanr:"ROLE-PLAY",
+				cena: "5000din"
+			} ]
+	}
+  ]
+  };
+  
+  console.log(igraonica); 
+}
